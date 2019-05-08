c6231129c1dc5c8eb6d61b73e7450b2e4335e4a1
Siegel <csiegel@dmu.edu>
2019-05-07 20:25:35 -0500

    added threatding so the main loop will keep running and looking fo incoming comments! (and becuase I thought it would be neat)

--- aita_counts/aita_counts_bot.py
+++ aita_counts/aita_counts_bot.py
@@ -1,5 +1,7 @@
 import re
 from collections import defaultdict
+import threading
+
 import config
 
 reddit = config.reddit_config
@@ -11,23 +13,7 @@ def generate_counts(submission_id):
     submission = reddit.submission(id=submission_id)
 
     aita_regex = re.compile(r'(YTA|NTA|ESH|NAH|INFO)')
-    sb = []
-
-    # top level comments
-    # aita_dict_top = defaultdict(int)
-    #
-    # for top_level_comment in submission.comments:
-    #     matcher = None
-    #     if hasattr(top_level_comment, 'body'):
-    #         matcher = aita_regex.search(top_level_comment.body)
-    #     if matcher:
-    #         aita_dict_top[matcher.group(1)] += 1
-    #
-    # total_votes_top = sum([v for v in aita_dict_top.values()])
-    #
-    # for k, v in aita_dict_top.items():
-    #     sb.append(f"{k} has {str((v/total_votes_top)*100).split('.')[0]}% of the top level comments with votes. ({v}/{total_votes_top})" + "\n\n")
-    # sb.append('\n\n&nbsp;')
+    bot_comment = []
 
 
     # all comments
@@ -42,24 +28,29 @@ def generate_counts(submission_id):
     total_votes_all = sum([v for v in aita_dict_all.values()])
 
     for k, v in aita_dict_all.items():
-        sb.append(f"{k} has {str((v/total_votes_all)*100).split('.')[0]}% of all of the comments with votes in"
+        bot_comment.append(f"{k} has {str((v/total_votes_all)*100).split('.')[0]}% of all of the comments with votes in"
                   f" this submisson. ({v}/{total_votes_all} total comments)" + "\n\n")
 
-    sb.append(
+    bot_comment.append(
         "\n\n^I ^am ^the ^AITA ^bot!  ^Post ^'!aita_bot' ^in ^any ^AmITheAsshole ^submission ^and ^I'll ^reply ^to ^your ^comment ^with ^counts ^and ^percentages"
         " ^of ^how ^users ^voted ^in ^this ^thread.")
-    sb.append('\n\n')
-    sb.append("^^Feel ^^free ^^to ^^message ^^the ^^creator, ^^/u/blarmon_kek ^^with ^^any ^^questions ^^or ^^concerns!")
+    bot_comment.append('\n\n')
+    bot_comment.append("^^Feel ^^free ^^to ^^message ^^the ^^creator, ^^/u/blarmon_kek ^^with ^^any ^^questions, ^^concerns, ^^or ^^suggestions!")
 
-    return ''.join(sb)
+    return ''.join(bot_comment)
 
-
+def threader(comment):
+    comment.reply(generate_counts(comment.submission.id))
+    return
 
 key_phrase = '!aita\_bot'
 
-while True:
-    for comment in subreddit.stream.comments(skip_existing=True):
-        print(comment.body)
-        if key_phrase in comment.body:
-            comment.reply(generate_counts(comment.submission.id))
+
+if __name__ == '__main__':
+    while True:
+        for comment in subreddit.stream.comments(skip_existing=True):
+            if key_phrase in comment.body:
+                t = threading.Thread(target=threader, args=(comment, ))
+                t.daemon = False
+                t.start()
 
