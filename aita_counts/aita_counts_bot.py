14aaa775d470947b157d373589e27e7a6113c68a
Siegel <csiegel@dmu.edu>
2019-05-09 21:17:05 -0500

    cleaned up my threading code a lot.  still lots of improvement to be made.

--- aita_counts/aita_counts_bot.py
+++ aita_counts/aita_counts_bot.py
@@ -1,56 +1,65 @@
 import re
 from collections import defaultdict
 import threading
+from queue import Queue
 
 import config
 
 reddit = config.reddit_config
+subreddit = reddit.subreddit('AmITheAsshole')
 
-subreddit = reddit.subreddit('AmItheAsshole')
-
+# TODO! maybe write successful comments out to a file with some metadata.  could be fun to have the data in json format down the line or something.
+# TODO.  grab the most upvoted comment for each type of vote and link to it.  that's good stuff.
+# TODO clean up variable shadowing
+# TODO error handling
 
 def generate_counts(submission_id):
     submission = reddit.submission(id=submission_id)
-
-    aita_regex = re.compile(r'(YTA|NTA|ESH|NAH|INFO)')
-    bot_comment = []
-
-
-    # all comments
-    aita_dict_all = defaultdict(int)
+    aita_dict = defaultdict(int)
 
     submission.comments.replace_more(limit=None)
     for comment in submission.comments.list():
         matcher = aita_regex.search(comment.body)
         if matcher:
-            aita_dict_all[matcher.group(1)] += 1
+            aita_dict[matcher.group(1)] += 1
 
-    total_votes_all = sum([v for v in aita_dict_all.values()])
+    total_votes_all = sum([v for v in aita_dict.values()])
+    bot_comment = []
 
-    for k, v in aita_dict_all.items():
+    for k, v in aita_dict.items():
         bot_comment.append(f"{k} has {str((v/total_votes_all)*100).split('.')[0]}% of all of the comments with votes in"
                   f" this submisson. ({v}/{total_votes_all} total comments)" + "\n\n")
 
     bot_comment.append(
-        "\n\n^I ^am ^the ^AITA ^bot!  ^Post ^'!aita_bot' ^in ^any ^AmITheAsshole ^submission ^and ^I'll ^reply ^to ^your ^comment ^with ^counts ^and ^percentages"
-        " ^of ^how ^users ^voted ^in ^this ^thread.")
+        "\n\n^I ^am ^the ^AITA ^bot!  ^Post ^'!aita_bot' ^in ^any ^AmITheAsshole ^submission ^and ^I'll ^reply ^to ^your"
+        " ^comment ^with ^counts ^and ^percentages ^of ^how ^users ^voted ^in ^this ^thread.")
     bot_comment.append('\n\n')
-    bot_comment.append("^^Feel ^^free ^^to ^^message ^^the ^^creator, ^^/u/blarmon_kek ^^with ^^any ^^questions, ^^concerns, ^^or ^^suggestions!")
+    bot_comment.append("^^Feel ^^free ^^to ^^message ^^the ^^creator, ^^/u/blarmon_kek ^^with ^^any ^^questions,"
+                       " ^^concerns, ^^or ^^suggestions!")
 
     return ''.join(bot_comment)
 
-def threader(comment):
-    comment.reply(generate_counts(comment.submission.id))
-    return
 
-key_phrase = '!aita\_bot'
+def threader():
+    while True:
+        if not q.empty():
+            comment = q.get()
+            comment.reply(generate_counts(comment.submission.id))
+            q.task_done()
 
 
 if __name__ == '__main__':
+
+    key_phrase = '!aita\_bot'
+    q = Queue()
+    aita_regex = re.compile(r'(YTA|NTA|ESH|NAH|INFO)')
+
+    for _ in range(5):
+        t = threading.Thread(target=threader)
+        t.daemon = False
+        t.start()
+
     while True:
         for comment in subreddit.stream.comments(skip_existing=True):
             if key_phrase in comment.body:
-                t = threading.Thread(target=threader, args=(comment, ))
-                t.daemon = False
-                t.start()
-
+                q.put(comment)
